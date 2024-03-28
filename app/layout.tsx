854827fe9e4e4955293f6cd7c56b57c3fd120129
Jose Barajas <jose.barajas@attackiq.com>
2024-03-28 12:23:41 -0400

    first push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,19 @@
+import '@/app/ui/global.css';
+import styles from '@/app/ui/home.module.css';
+<div className={styles.shape} />;
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
   );
 }
+
+
+
